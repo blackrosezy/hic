139b2972068ab3885932bfea2217c4e8b44eeee4
Mohd Rozi <blackrosezy@gmail.com>
2015-02-04 05:28:37 +1000

    fixed missing dependencies in setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,19 @@ setup(
     version='2.0.0',
     packages=['hic'],
     url='https://github.com/blackrosezy/hic',
-    license='MIT',
+    license='MIT, see LICENSE file',
     author='MohdRozi',
     author_email='blackrosezy@gmail.com',
     description='Hipache cli tool',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'docopt==0.6.2',
+        'docker-py==0.5.3',
+        'redis==2.10.3',
+        'tabulate==0.7.3'
+    ],
     entry_points={
         'console_scripts': ['hic=hic.hic:main'],
-    }
+    },
+    zip_safe=False
 )
